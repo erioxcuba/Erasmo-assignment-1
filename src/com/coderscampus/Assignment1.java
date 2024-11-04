cbcf098fc2f915463612b6b0c5094659df19640c
Erasmo <erioxcuba@gmail.com>
2024-11-04 13:23:03 -0600

    ended

--- src/com/coderscampus/Assignment1.java
+++ src/com/coderscampus/Assignment1.java
@@ -1,10 +1,86 @@
 package com.coderscampus;
 
+import java.util.Scanner;
+
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        Scanner scanner = new Scanner(System.in);		
+
+        Double  initAmount = null;
+        
+        while (initAmount == null)
+        	
+        {
+    		System.out.println("Enter the initial amount, greater than 0 and not greater than 1000000");
+    		
+            String initAmountInput = scanner.nextLine(); // Read a line of input
+            
+            initAmount = validateInitAmount (initAmountInput);
+            
+            if (initAmount == null)
+            	
+            {
+        		System.out.println("your number doesnt comply with the requirements");
+            }           
+        }  
+
+        Double  yearInterestRate = null;
+        
+        while (yearInterestRate == null)
+        	
+        {
+    		System.out.println("Enter the year interest rate, greater than 0 and not greater than 1");
+    		
+            String yearInterestInput = scanner.nextLine(); // Read a line of input
+            
+            yearInterestRate = validateInterest (yearInterestInput);
+            
+            if (initAmount == null)
+            	
+            	{
+        		System.out.println("your number doesnt comply with the requirements");
+            	}           
+        }  
+        
+        double compAmount = initAmount;
+        
+        for (var i =0; i< 6; i++)
+        {
+        	compAmount = calcCompoundingInterest (compAmount, yearInterestRate);
+       		
+            System.out.println("After " + i*5 + " years, you would have: " + Math.round(compAmount));
+        }
+        
+        scanner.close();
 
 	}
+	
+	static double calcCompoundingInterest (double initAmount, double yearInterest)
+	{
+		double compAmount = initAmount;
+		
+		for (var i = 0; i < 5; i++)
+		{
+			compAmount *= (yearInterest + 1);
+		}
+		
+		return compAmount;
+	}
+	
+	static Double validateInitAmount (String initAmountInput )
+	{
+		Double initAmount= Double.parseDouble(initAmountInput);
+		
+	    return (initAmount < 0 || initAmount > 1000000) ? null : initAmount;	
 
+	}
+	
+	static Double validateInterest (String yearInterestInput )
+	{
+		Double yearInterest= Double.parseDouble(yearInterestInput);
+		
+	    return (yearInterest < 0 || yearInterest > 1) ? null : yearInterest;
+	}
 }
